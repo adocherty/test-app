774a455b655c4c4541b86e28d8ca4fe9885accac
Andrew Docherty <docherty+gh@gmail.com>
2025-01-15 02:07:18 +0000

    Added multiple pages & settings test

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,39 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+
+st.set_page_config(
+    page_title="AD's streamlit test",
+    page_icon=":material/bug_report:",
 )
+
+st.title("🎈 My new app")
+
+openai_api_key = st.sidebar.text_input("OpenAI API Key", type="password")
+
+
+def info():
+    st.write("## Info page")
+    st.write(f"OpenAI API key: {openai_api_key}")
+
+    setting_1 = st.session_state['setting_1']
+    setting_2 = st.session_state['setting_2']
+
+    st.write(f"Setting 1: {setting_1}")
+    st.write(f"Setting 2: {setting_2}")
+
+
+def settings():
+    st.write("## Settings page")
+    setting_1 = st.checkbox("Setting 1")
+    setting_2 = st.checkbox("Setting 2")
+    st.session_state['setting_1'] = setting_1
+    st.session_state['setting_2'] = setting_2
+
+
+# Define some pages
+info_page = st.Page(info, title="Info", icon=":material/info:")
+settings_page = st.Page(settings, title="Settings", icon=":material/settings:")
+
+pg = st.navigation([settings_page, info_page])
+
+pg.run()
